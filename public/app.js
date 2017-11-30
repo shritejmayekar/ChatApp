5acbb659ac37ef7557d9c255bea93b3b109eff1b
shritejmayekar <shritejmayekar@gmail.com>
2017-11-30 17:17:50 +0530

    session refresh corrected

--- public/app.js
+++ public/app.js
@@ -1,22 +1,102 @@
-var app = angular.module('myChatApp',[]);
-app.controller('myController',['$scope',function($scope){
-var socket = io.connect();
+var app = angular.module('myChatApp', []);
+app.controller('myController', ['$scope', '$http', function($scope, $http) {
+  var socket = io.connect();
+  var username;
+  var password;
+  var status;
+  //login loginArea
+  $scope.login = function() {
+    if ($scope.name == "" || $scope.name == null || $scope.password == '' || $scope.password == null) {
+      document.getElementById('flag').innerHTML = "<p style='color:red'>*Fields are blanks</p>"
+      return false;
+    }
+    console.log($scope.name);
+    username = $scope.name;
+    password = $scope.password;
+    $http({
+      method: 'POST',
+      url: '/login',
+      data: {
+        name: $scope.name,
+        password: $scope.password
+      }
+    }).then(function(res) {
+      if (res.data.status == 'logged in') {
+        $('#loginArea').hide();
+        $('#messageArea').show();
+        $('#showhide').show();
+        document.getElementById('userLogin').innerHTML = "Welcome," + res.data.name;
 
-$scope.send = function() {
-  var dt=new Date();
-  var ntime=dt.getHours()+":"+dt.getMinutes();
-  console.log('k'+dt.getTime());
-  $scope.messages=$scope.messages+" "+ntime;
-  $scope.time=ntime;
-  socket.emit('chat message',$scope.messages);
-  $scope.messages='';
+        //new userArea
+        username = res.data.name;
+        console.log(username);
+        socket.emit('new user', $scope.name, function() {
 
-}
+        });
 
-  socket.on('chat message',function(msg) {
-      var li=document.createElement('li');
-      li.appendChild(document.createTextNode(msg));
-      document.getElementById('message').appendChild(li);
+      } else {
+        document.getElementById('flag').innerHTML = "<p style='color:red'>name or password is wrong</p>"
+      }
+
+    }, function(err) {
+
+    })
+
+  }
+
+
+
+
+
+  //new userArea
+
+  $scope.sendUser = function() {
+    if ($scope.newUser == "" || $scope.newUser == null) {
+      return false;
+    }
+    socket.emit('new user', $scope.newUser, function() {
+
+    });
+    $scope.newUser = '';
+  }
+
+  //send messageArea
+  $scope.send = function() {
+    if ($scope.messages == '' || $scope.messages == null) {
+      return false;
+    }
+
+    // get current time
+    var dt = new Date();
+    var ntime = dt.getHours() + ":" + dt.getMinutes();
+    // append message with time
+    $scope.messages = $scope.messages + " " + ntime;
+    $scope.time = ntime;
+    console.log(username);
+    username = (username == undefined) ? sessioname : username;
+    // call event chat message socket io
+    socket.emit('chat message', {
+      username: username,
+      msg: $scope.messages
+    });
+    $scope.messages = '';
+
+  }
+  // get new message display
+  socket.on('new message', function(data) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(data.username + ': ' + data.msg));
+    document.getElementById('message').appendChild(li);
+  });
+
+  //get user names and display
+  socket.on('get users', function(data) {
+    document.getElementById('activeUser').innerHTML = "";
+    for (var i = 0; i < data.length; i++) {
+      var li = document.createElement('li');
+      li.appendChild(document.createTextNode(data[i]));
+      document.getElementById('activeUser').appendChild(li);
+    }
   });
 
 }]);
--- server.js
+++ server.js
@@ -1,32 +1,161 @@
+/**
+@author shritej
+@since 22-09-2017
+*/
+//Deaclaration
 var express = require('express');
-//var app = require('express')();
-var clients=0;
+var clients = 0;
+var sessName;
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var session = require('express-session')
 app.use(express.static('public'));
-app.get('/', function(request, response) {
+//session userArea
+app.use(session({
+  secret: 'phantom'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+//declaring variable sess for session save as temporary.
+var sess;
 
-  //response.sendFile('/home/bridgelabz/shritej/chatApp/public/index.html');
- 	//app.use(express('public'));
-  response.redirect("index.html");
+app.get('/', function(req, res) {
+  sess = req.session;
+  sessName = req.session.name;
+  if (sess.name) {
+    //if exists the session
+    console.log(sess.name);
+
+    res.send({
+      status: 'logged in'
+    })
+  } else {
+
+    res.send({
+      status: 'not logged in'
+    })
+  }
 
 });
 
+
+app.post('/login', function(req, res) {
+  sess = req.session;
+  sess.name = req.body.name;
+  if (sess.name == null || sess.name == '' || sess.name == undefined) {
+    res.send({
+      status: 'not logged in'
+
+    })
+  } else {
+    res.send({
+      status: 'logged in',
+      name: sess.name
+    })
+  }
+
+});
+app.get('/admin', function(req, res) {
+  sess = req.session;
+  console.log(sess);
+  if (sess.name) {
+    res.write('<h1>Hello ' + sess.name + '</h1>');
+    res.end('<a href="/logout">Logout</a>');
+  } else {
+    res.write('<h1>Please Login first.</h1>');
+    res.end('<a href="/">Login</a>');
+  }
+
+});
+app.get('/checkSession', function(req, res) {
+  var result = {
+    status: true,
+    message: "You are authorized",
+    name: req.session.name
+  }
+  if (req.session && req.session.name) {
+    result.data = {
+      isLogin: true
+    }
+  } else {
+    result.data = {
+      isLogin: false
+    }
+  }
+
+  res.send(result);
+
+});
+app.get('/user', function(req, res) {
+  var result;
+  if (req.session && req.session.name) {
+
+    result = req.session.name
+
+    res.send(result);
+  } else {
+    result = null;
+    res.send(result);
+  }
+
+});
+
+app.get('/logout', function(req, res) {
+  req.session.destroy(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+//Declare socket
+var users = [];
+var socketId = [];
+
+//create socket
 io.on('connection', function(socket) {
   clients++;
-  socket.on('chat message', function(msg) {
-    console.log('message:' + msg);
-    io.emit('chat message', msg);
-
+  console.log('Clients connected:' + clients);
+  console.log(socket.id);
+  // send message
+  socketId.push(socket.id);
+  socket.on('chat message', function(data) {
+    // broadcast message to clients/users
+    io.emit('new message', data);
   });
-
+  //Disconnect
   socket.on('disconnect', function(socket) {
     clients--;
+
+    users.splice(users.indexOf(socket), 1);
+    console.log(socket.newUser);
     console.log("user is disconnected");
+    console.log('Clients connected:' + clients);
+    updateUsernames();
   });
+  //new user
+  socket.on('new user', function(data, callback) {
+    callback(true);
+    socket.newUser = data;
+    if (users.indexOf(socket.newUser) < 0)
+      users.push(socket.newUser);
+    updateUsernames();
+  });
+  // updates user name based on connected and disconnected
+  function updateUsernames() {
+    //broadcast the username to all
+    io.emit('get users', users);
+  }
+
+
 });
 
+// listen port
 http.listen(3000, function(err) {
   if (err) throw err;
   console.log('listening on *:3000');
